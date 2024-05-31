7119bb58105dea81d1beca7c6af2a77cc97f4245
Paul Meidinger <pmeidinger@lodiusd.net>
2024-05-31 13:38:49 -0700

    IDE1005: Use conditional delegate call

--- PCLParaphernalia/PCLCharCollItem.cs
+++ PCLParaphernalia/PCLCharCollItem.cs
@@ -85,10 +85,7 @@ namespace PCLParaphernalia
 
         private void OnPropertyChanged(string propertyName)
         {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
