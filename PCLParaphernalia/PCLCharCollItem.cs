ae1af754534f73b725e66e407d6ca4e96c2095ec
Paul Meidinger <pmeidinger@lodiusd.net>
2025-07-01 12:01:46 -0700

    Simplify delegate invocation

--- PCLParaphernalia/PCLCharCollItem.cs
+++ PCLParaphernalia/PCLCharCollItem.cs
@@ -115,12 +115,7 @@ namespace PCLParaphernalia
 
         private void onPropertyChanged(string propertyName)
         {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(
-                    this,
-                    new PropertyChangedEventArgs(propertyName));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
